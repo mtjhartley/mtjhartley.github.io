bb39fb516d99684a3ce53ed557583997fde54342
Michael Hartley <mtjhartley@gmail.com>
2017-08-14 18:46:42 -0700

    google form submission

---
+++ js/gform.js
@@ -0,0 +1,8 @@
+<script src="../vendor/jquery/jquery.min.js"></script>
+<script type="text/javascript">var submitted=false;</script>
+<script type="text/javascript">
+$('#gform').on('submit', function(e) {
+  $('#gform *').fadeOut(2000);
+  $('#gform').prepend('Your submission has been processed...');
+  });
+</script>
